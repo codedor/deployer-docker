5e275da274fd561ddfce21c626432611549d909d
Michiel Bosman <michiel.bosman@onlyhumans.com>
2021-01-18 16:14:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo "Hello $1"
 mkdir ~/.ssh && echo "$1" > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
 ulimit -n "231070"
 rm -rf node_modules package-lock.json && npm cache clear --force
-dep deploy $2
+/usr/local/bin/dep deploy -v $2
